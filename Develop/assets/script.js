64b25a684669560ea5463d1a72fcb1396ec2fa71
Jason Jones <jason.e.jones@gmail.com>
2021-02-24 18:27:23 -0500

    all dynamic HTML

---
+++ Develop/assets/script.js
@@ -0,0 +1,172 @@
+$('#currentDay').append(moment().format('MMMM Do YYYY')); 
+
+const calStartHour = 9;
+const calEndHour = 17;
+var timeText;
+
+var myCalendar = $("#myCalendar");
+
+// var container = $("<div>");
+// container.addClass("myCalendar");
+// $("body").append(container);
+
+// var row = $("<div>");
+// row.addClass("row");
+// $("body").append(row);
+
+// var cellOne = $("<div>");
+// cellOne.addClass("col-2 border");
+// cellOne.text("9am");
+// $("body").append(cellOne);
+
+
+// var cellTwo = $("<div>");
+// cellTwo.addClass("col-9 border");
+// cellTwo.text("imma edit you eventually");
+// $("body").append(cellTwo);
+
+// var cellThree = $("<div>");
+// cellThree.addClass("col-1 border");
+// cellThree.text("save");
+// $("body").append(cellThree);
+
+// row.append(cellOne, cellTwo, cellThree);
+// myCalendar.append(row);
+// $("body").append(html);
+// its just an input or a textarea
+// var timeRow = '<div class="container"><div class="row"><div class="col-2 border">XPM</div><div class="col-9 border">getting somewhere</div><div class="col-1 border">SV</div></div></div>';
+// $('#myCalendar').append(timeRow);
+// var timeRow = '<div class="container"><div class="row"><div class="col-2 border">XPM</div><div class="col-9 border">getting somewhere</div><div class="col-1 border">SV</div></div></div>';
+// $('#myCalendar').append(timeRow);
+
+// var cell1 = 
+
+
+
+
+
+for (var i = 9; i <= 17; i++) {  
+    if (i < 12) {
+        timeText=i+"AM";
+    } 
+    if (i == 12) {
+        timeText="12PM";
+    } 
+    if (i > 12) { // i > 12
+        timeText=(i-12)+"PM";
+    }
+var row = $("<div>");
+row.addClass("row");
+// $("body").append(row);
+
+var cellOne = $("<div>");
+cellOne.addClass("col-2 border");
+cellOne.text(timeText);
+// $("body").append(cellOne);
+
+
+var cellTwo = $("<div>");
+cellTwo.addClass("col-9 border");
+cellTwo.text("imma edit you eventually");
+// $("body").append(cellTwo);
+
+var cellThree = $("<div>");
+cellThree.addClass("col-1 border");
+cellThree.text("save");
+// $("body").append(cellThree);
+
+row.append(cellOne, cellTwo, cellThree);
+myCalendar.append(row);
+//     $('#calendar-area').append("<h1>"+timeText+"</h1>");
+}
+
+
+
+
+// const $tableID = $('#table');
+//  const $BTN = $('#export-btn');
+//  const $EXPORT = $('#export');
+
+//  const newTr = `
+// <tr class="hide">
+//   <td class="pt-3-half" contenteditable="true">Example</td>
+//   <td class="pt-3-half" contenteditable="true">Example</td>
+//   <td class="pt-3-half" contenteditable="true">Example</td>
+//   <td class="pt-3-half" contenteditable="true">Example</td>
+//   <td class="pt-3-half" contenteditable="true">Example</td>
+//   <td class="pt-3-half">
+//     <span class="table-up"><a href="#!" class="indigo-text"><i class="fas fa-long-arrow-alt-up" aria-hidden="true"></i></a></span>
+//     <span class="table-down"><a href="#!" class="indigo-text"><i class="fas fa-long-arrow-alt-down" aria-hidden="true"></i></a></span>
+//   </td>
+//   <td>
+//     <span class="table-remove"><button type="button" class="btn btn-danger btn-rounded btn-sm my-0 waves-effect waves-light">Remove</button></span>
+//   </td>
+// </tr>`;
+
+//  $('.table-add').on('click', 'i', () => {
+
+//    const $clone = $tableID.find('tbody tr').last().clone(true).removeClass('hide table-line');
+
+//    if ($tableID.find('tbody tr').length === 0) {
+
+//      $('tbody').append(newTr);
+//    }
+
+//    $tableID.find('table').append($clone);
+//  });
+
+//  $tableID.on('click', '.table-remove', function () {
+
+//    $(this).parents('tr').detach();
+//  });
+
+//  $tableID.on('click', '.table-up', function () {
+
+//    const $row = $(this).parents('tr');
+
+//    if ($row.index() === 0) {
+//      return;
+//    }
+
+//    $row.prev().before($row.get(0));
+//  });
+
+//  $tableID.on('click', '.table-down', function () {
+
+//    const $row = $(this).parents('tr');
+//    $row.next().after($row.get(0));
+//  });
+
+//  // A few jQuery helpers for exporting only
+//  jQuery.fn.pop = [].pop;
+//  jQuery.fn.shift = [].shift;
+
+//  $BTN.on('click', () => {
+
+//    const $rows = $tableID.find('tr:not(:hidden)');
+//    const headers = [];
+//    const data = [];
+
+//    // Get the headers (add special header logic here)
+//    $($rows.shift()).find('th:not(:empty)').each(function () {
+
+//      headers.push($(this).text().toLowerCase());
+//    });
+
+//    // Turn all existing rows into a loopable array
+//    $rows.each(function () {
+//      const $td = $(this).find('td');
+//      const h = {};
+
+//      // Use the headers from earlier to name our hash keys
+//      headers.forEach((header, i) => {
+
+//        h[header] = $td.eq(i).text();
+//      });
+
+//      data.push(h);
+//    });
+
+//    // Output the result
+//    $EXPORT.text(JSON.stringify(data));
+//  });
