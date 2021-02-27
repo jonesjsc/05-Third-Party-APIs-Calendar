70ced9c2d8dc5217aa6e6c18032e27791de6a8b6
Jason Jones <jason.e.jones@gmail.com>
2021-02-27 15:51:36 -0500

    Pretty much working just need to clean this up

--- Develop/assets/script2.js
+++ Develop/assets/script2.js
@@ -1,10 +1,10 @@
+// Simple Calendar app
+// moment.js
+// DOM manipulation / traversal
+
 $('#currentDay').append(moment().format('MMMM Do YYYY')); 
 
-const calStartHour = 9;
-const calEndHour = 17;
-var timeText;
 var myCalendar = $("#myCalendar");
-
 
 class calData {
     constructor(timeslot, appt) {
@@ -26,13 +26,13 @@ class Store {
                 {timeslot: "11am",
                  appt: ""},
                 {timeslot: "12pm",
-                 appt: "some appt"},
+                 appt: ""},
                 {timeslot: "1pm",
                  appt: ""},
                 {timeslot: "2pm",
                  appt: ""},
                 {timeslot: "3pm",
-                 appt: "a different appt"},
+                 appt: ""},
                 {timeslot: "4pm",
                  appt: ""},
                 {timeslot: "5pm",
@@ -62,103 +62,113 @@ class Store {
         localStorage.setItem('calData', JSON.stringify(calData));
     }
 
-    static updateCalEntry(timeSlice, apptData) {
+    
         // when called we're expecting the timeSlice from the text area 9AM, 10AM etc
         // and we need the data from the text area so we can find this in the 
         // calData localStorage array and update that
+    
+    static updateWhatChanged(apptTime,apptText) {
+        const myCalData = Store.getCalData();
+        myCalData.forEach((calEvent, index) => {
+            if (calEvent.timeslot == apptTime) {
+                calEvent.appt=apptText;
+            }
+            localStorage.setItem('calData', JSON.stringify(myCalData));
+         });
     }
 
 }
-
-
 
 class UI {
     static displayCal () {
 
-    const myCalData = Store.getCalData();
-    console.log ("this is UI.displayCal here and this is myCalData ",myCalData);
+        const myCalData = Store.getCalData();
 
-    myCalData.forEach((calEvent, index) => {
+        myCalData.forEach((calEvent, index) => {
 
-        var timeToCheck=index+9;
+            var timeToCheck=index+9; // kinda being lazy - this array represetns time that starts at 9am
+            var row = $("<div>");
+            row.addClass("row");
+
+            // build the individual cell divs
+
+            var cellOne = $("<div>");
+            cellOne.addClass("col-1 border pt-2 pb-2");
+            cellOne.text(calEvent.timeslot);
+
+            // cellTwo has to be a FORM
+            //
+            // <form>
+            //    <input type="text"
+            //    id="calText">
+            // 
+            // var cellTwo = $("<div>");
+
+            var cellTwo = $("<form>");
+            cellTwo.addClass("col-10 border pt-2 pb-2");
+            
+            // var cellTwoDiv = $("<div>");
+            
+            var cellTwoInput = $("<input>");
+            cellTwoInput.addClass("form-control");
+            cellTwoInput.attr({type: "text", id: "calText", value: calEvent.appt});
+            
+            // lets colarize the calendar view based on the current time
+            
+            var currentHour=moment().format("H");
+            
+            if (currentHour > timeToCheck) {
+                cellTwo.addClass("bg-secondary");
+                cellTwoInput.addClass("bg-secondary text-white");
+            }
+            if (currentHour == timeToCheck) {
+                cellTwo.addClass("bg-danger");
+                cellTwoInput.addClass("bg-danger text-white");
+            }
+            if (currentHour < timeToCheck) {
+                cellTwo.addClass("bg-success");
+                cellTwoInput.addClass("bg-success text-white");
+            }
+            cellTwo.append(cellTwoInput);
         
-        console.log (calEvent.timeslot, calEvent.appt);
-    
-        var row = $("<div>");
-        row.addClass("row");
+            var cellThree = $("<div>");
+            cellThree.addClass("col-1 border pt-2 pb-2 btn btn-sm saveBtn");
+            cellThree.text("save"); // use font awesome here
 
-        // build the individual cell divs
+            // put these cell divs into the row div
 
-        var cellOne = $("<div>");
-        cellOne.addClass("col-1 border pt-2 pb-2");
-        cellOne.text(calEvent.timeslot);
+            row.append(cellOne, cellTwo, cellThree);
+            
+            // append the whole row div into the myCalendar div
+            
+            myCalendar.append(row);
 
-        // cellTwo has to be a FORM
-        //
-        // <form>
-        //    <input type="text"
-        //    id="calText">
-        // 
-        // var cellTwo = $("<div>");
-
-        var cellTwo = $("<form>");
-        cellTwo.addClass("col-10 border pt-2 pb-2");
-        
-        // var cellTwoDiv = $("<div>");
-        
-        var cellTwoInput = $("<input>");
-        cellTwoInput.addClass("form-control");
-        cellTwoInput.attr({type: "text", id: "calText", value: calEvent.appt});
-        
-        // lets colarize the calendar view based on the current time
-        
-        var currentHour=moment().format("H");
-        currentHour=12;
-
-        if (currentHour > timeToCheck) {
-            cellTwo.addClass("bg-secondary");
-            cellTwoInput.addClass("bg-secondary text-white");
-        }
-        if (currentHour == timeToCheck) {
-            cellTwo.addClass("bg-danger");
-            cellTwoInput.addClass("bg-danger text-white");
-        }
-        if (currentHour < timeToCheck) {
-            cellTwo.addClass("bg-success");
-            cellTwoInput.addClass("bg-success text-white");
-        }
-        cellTwo.append(cellTwoInput);
-      
-        var cellThree = $("<div>");
-        cellThree.addClass("col-1 border pt-2 pb-2 saveBtn");
-        // callThree.setAttribute("id", "saveBtn");
-        cellThree.text("save");
-
-        // put these cell divs into the row div
-
-        row.append(cellOne, cellTwo, cellThree);
-        
-        // append the whole row div into the myCalendar div
-        
-        myCalendar.append(row);
-
-    });
+        });
     }
+
+    static locateWhatChanged(event) {
+        if(event.target.classList.contains('saveBtn')) {  // lets be sure the click is on the save area
+        
+            // DOM traversial is so much fun.  
+            // I heart DOM traversial
+            
+            var apptText = (event.target.previousSibling.childNodes[0].value); 
+            
+            var apptTime = (event.target.previousSibling.previousSibling.textContent);
+            console.log ("i need to update "+apptTime+" and "+apptText);
+            Store.updateWhatChanged(apptTime,apptText);
+        
+        }
+    }
+
+    // console.log ("i need to update "+apptTime+" and "+apptText);
 }
+// end UI Class defination
 
 document.querySelector('#myCalendar').addEventListener('click', (event) => {
     
-    if(event.target.classList.contains('saveBtn')) {  // lets be sure the click is on the save area
-        
-        // DOM traversial is so much fun.  
-        // I heart DOM traversial
-        
-        var apptText = (event.target.previousSibling.childNodes[0].value); 
-        
-        var apptTime = (event.target.previousSibling.previousSibling.textContent);
-        console.log ("i need to update "+apptTime+" and "+apptText);
-    }
-    // Store.updateCalEntry(e.target.parentElement.previousElementSibling.textContent);
+    UI.locateWhatChanged(event);
+
 });
 
 UI.displayCal ();
